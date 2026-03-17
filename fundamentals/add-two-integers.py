57c1ee35cfd533d806d4e943801fdf051250b97a
vijaypingale <vijay.pingale@gmail.com>
2026-03-17 17:14:01 -0400

    python fundamental- add-two-numbers and user-input

---
+++ fundamentals/add-two-integers.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 20
+
+print(f"Addition of two numbers are : {number1+number2}")
+
+## Take two number and print their sum
+number1 = int(input("Enter 1st number: "))
+number2 = int(input("Enter 2nd number: "))
+
+print(f"Sum of entered two numbers are: = {number1+number2}")
