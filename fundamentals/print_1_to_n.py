534ed0018add9a42c804e2baf944f3140296aba5
vijaypingale <vijay.pingale@gmail.com>
2026-03-17 19:06:03 -0400

    Adding program to print 1 to n using for loop

---
+++ fundamentals/print_1_to_n.py
@@ -0,0 +1,11 @@
+# print_1_to_n
+
+def print_1_to_n(n):
+    for i in range(1, n+1):
+        print(i)
+
+take_n = int(input("Enter number to print n numbers: "))
+
+result = print_1_to_n(take_n)
+
+##print(result)
