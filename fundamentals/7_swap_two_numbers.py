f44543d14475b9df3f5ba5c95bc24009c93accd1
vijaypingale <vijay.pingale@gmail.com>
2026-03-21 16:17:56 -0400

    added miles to kilomerter program

--- fundamentals/7_swap_two_numbers.py
+++ fundamentals/7_swap_two_numbers.py
@@ -1,5 +1,7 @@
 # Write a program to swap a two numbers
 
+import random
+
 def swap_two_numbers(num1, num2):
     temp = num1
     num1 = num2
@@ -8,3 +10,7 @@ def swap_two_numbers(num1, num2):
     return num1, num2
 
 print(swap_two_numbers(8,2))
+text = "Test string Good afternoon"
+print(text.split()[1])
+
+print(f"Random number using ramdon functino: {random.randint(1,50)}")
